559d71b1c505ccf9dd611eda0da5a6db3e842c77
Milad Rastian <eslashmili@gmail.com>
2019-02-01 17:32:21 +0100

    Cleanup warnings

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use actix::prelude::System;
-use dockworker::{
-    container::AttachContainer, ContainerCreateOptions, ContainerLogOptions, CreateExecOptions,
-    Docker, StartExecOptions,
-};
-use pipewerk::{config, runner, scheduler};
-use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
-
-use std::time::Duration;
+use dockworker::Docker;
+use pipewerk::{config, scheduler};
 
 use std::fs;
 
@@ -16,7 +9,7 @@ fn main() {
     let f = fs::read_to_string("first-pipewerk.yml").unwrap();
     let config = config::Config::from_str(&f).unwrap();
 
-    let docker = Docker::connect_with_defaults().unwrap();
+    let _docker = Docker::connect_with_defaults().unwrap();
     let scheduler = scheduler::Scheduler::new(&config.jobs[0]);
     scheduler
         .try_send(scheduler::Message::RunJobs)
